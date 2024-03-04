99f97deb3382f7546e12daa185815bfad5000780
Sohaib Butt <sohaib.butt@txlabz.com>
2024-03-04 14:39:51 +0500

    Contact

--- src/components/Intro/Intro.jsx
+++ src/components/Intro/Intro.jsx
@@ -5,7 +5,7 @@ const Intro = () => {
   return (
     <div className="w-full py-20 sm:py-20 px-5 sm:pl-10 flex flex-col justify-center gap-5 sm:flex-row sm:items-center sm:justify-start">
       <p className="w-full sm:w-[70%] font-roboto font-[400] text-[8vw] sm:text-[42px] text-white leading-snug">
-        Here's <span className="tracking-[5px]">SHM</span>
+        I'm <span className="tracking-[5px]">SHM</span>
         <span className="text-[#4bffa5] tracking-[5px]">GILLANI</span>, a MERN
         Stack Developer turning ideas into{" "}
         <span className="text-[#4bffa5]">seamless</span> web
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,16 +1,18 @@
-import React from 'react'
-import Header from '../components/Header'
-import Intro from '../components/Intro/Intro'
-import AboutMe from '../components/AboutMe/AboutMe'
+import React from "react";
+import Header from "../components/Header";
+import Intro from "../components/Intro/Intro";
+import AboutMe from "../components/AboutMe/AboutMe";
+import Contact from "../components/Contact/Contact";
 
 const Home = () => {
   return (
-    <div className='w-full h-full pb-10 bg-slate-900'>
-      <Header/>
-      <Intro/>
-      <AboutMe/>
+    <div className="w-full h-full pb-10 bg-slate-900">
+      <Header />
+      <Intro />
+      <AboutMe />
+      <Contact />
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
