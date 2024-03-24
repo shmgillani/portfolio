03620acdd98d5b4d7cdf388c8c8c316f9ec5f50e
Syed Hassan Mehdi <syedhassanmehdi1999@gmail.com>
2024-03-24 19:17:40 +0500

    react-slick slider completed

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -1,14 +1,29 @@
 import React from "react";
 import "../App.css";
-import appreci from "../assets/images/appreci1.JPG";
+import appreci from "../assets/images/appreci.svg";
 import muallimu from "../assets/images/muallim.webp";
 import muallimu1 from "../assets/images/muallim1.webp";
 import muallimu2 from "../assets/images/muallim2.webp";
 import muallimu3 from "../assets/images/muallim3.webp";
 import muallimu4 from "../assets/images/muallim4.webp";
 import muallimu5 from "../assets/images/muallim5.webp";
+import swoodle from "../assets/images/swoodle.svg"
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 const Work = () => {
+  var settings = {
+    dots: true,
+    // dotsClass: "text-[#4bffa5]",
+    arrows: true,
+    infinite: true,
+    autoplay: true,
+    speed: 500,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+  };
+
   const projects = [
     {
       title: "01 Muallimu Qurani",
@@ -28,31 +43,31 @@ const Work = () => {
       title: "03 Swoodle",
       description:
         "I have worked as a React js developer, converted the figma into a seamless and responsive website.",
-      introImg: appreci,
+      introImg: swoodle,
       detailedImages: [],
-    }
+    },
   ];
   return (
-    <div className="w-full px-5 sm:px-10">
+    <div className="w-full px-5 sm:px-10 flex flex-col gap-5">
       <div className="w-full font-roboto font-[400] text-[18px] sm:text-[28px] text-white py-2">
         <span className="text-[#4bffa5]">Work</span>
       </div>
       <div className="divider"></div>
-      <div className="pt-5 flex flex-col gap-3 sm:flex-row sm:gap-20">
-        {projects?.map((project) => (
-          <div className="w-full sm:w-[50%] md:w-[30%] flex flex-col gap-2">
-            <p className="w-full font-roboto font-[400] text-[22px] sm:text-[28px] text-white">
-              {project?.title}
-            </p>
-            <div className="divider"></div>
-            <img
-              src={project?.introImg}
-              alt="project img"
-              className="w-[100%] h-[600px] mt-3 rounded-[10px]"
-            />
-          </div>
-        ))}
-      </div>
+        <Slider className="work-slick-dots" {...settings}>
+          {projects?.map((project) => (
+            <div className="w-full ">
+              <p className="w-full font-roboto font-[400] sm:text-[16px] md:text-[20px] lg:text-[28px] text-white">
+                {project?.title}
+              </p>
+              <div className="divider"></div>
+              <img
+                src={project?.introImg}
+                alt="project img"
+                className="w-[100%] max-h-[400px] mt-3 rounded-[10px]"
+              />
+            </div>
+          ))}
+        </Slider>
     </div>
   );
 };
